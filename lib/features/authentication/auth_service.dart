225a1e550919a0c2e4e30ab8aadc48660b113e9f
Taif Rahim <taifrahim@pm.me>
2025-05-11 16:13:58 +0200

    added loglytics package and a sign out method

--- lib/features/authentication/auth_service.dart
+++ lib/features/authentication/auth_service.dart
@@ -12,9 +12,15 @@ class AuthService {
   final SupabaseClient supabaseClient;
 
   AuthService({required this.supabaseClient});
-  Future<AuthResponseModel> registerEmailAndPassword({required String email, required String password}) async {
+  Future<AuthResponseModel> registerEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
     try {
-      final AuthResponse response = await supabaseClient.auth.signUp(email: email, password: password);
+      final AuthResponse response = await supabaseClient.auth.signUp(
+        email: email,
+        password: password,
+      );
       final User? user = response.user;
       final Session? session = response.session;
 
@@ -28,15 +34,31 @@ class AuthService {
     }
   }
 
-  Future<AuthResponseModel> loginWithEmailAndPassword({required String email, required String password}) async {
+  Future<AuthResponseModel> loginWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
     try {
-      final AuthResponse response = await supabaseClient.auth.signInWithPassword(email: email, password: password);
+      final AuthResponse response = await supabaseClient.auth.signInWithPassword(
+        email: email,
+        password: password,
+      );
       final User? user = response.user;
       final Session? session = response.session;
 
       return AuthResponseModel(session: session, user: user);
     } on FormatException {
       rethrow;
+    } on AuthException {
+      rethrow;
+    } on Exception {
+      rethrow;
+    }
+  }
+
+  Future<void> signOut() async {
+    try {
+      await supabaseClient.auth.signOut();
     } on AuthException {
       rethrow;
     } on Exception {
--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,11 @@ Future<void> main() async {
   );
   AuthService authService = AuthService(supabaseClient: Supabase.instance.client);
   final result = await authService.registerEmailAndPassword(
-    email: "example email here",
-    password: "example ",
+    email: "taifrahim@pm.me",
+    password: "12345678!",
   );
 
-  log("User with email:  ${result.user?.email} create successfully!");
+  log("User with email:  ${result.user?.email} signed in successfully!");
   runApp(MyApp());
 }
 
@@ -54,7 +54,10 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(backgroundColor: Theme.of(context).colorScheme.inversePrimary, title: Text(widget.title)),
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
